cc69b01100fa43c317cf301b90ce1a2051ab9be2
Mostafa Kreidly <mostafa@Mostafas-MacBook-Air.local>
2023-07-30 01:54:29 +0300

    <BE> changed migration, did some table and file restructionring

--- pet-store/database/migrations/2014_10_12_000000_create_users_table.php
+++ pet-store/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,42 @@ return new class extends Migration {
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            // $table->rememberToken();
-            $table->timestamps();
-            $table->integer('string');
+            $table->string('role');
 
         });
+
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string("category");
+            $table->string("tags");
+        });
+
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->string("desciption");
+            $table->decimal("price");
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+
+
+
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+        });
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('cart_id');
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('cart_id')->references('id')->on('carts')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+
+
     }
 
     /**
@@ -29,5 +60,9 @@ return new class extends Migration {
     public function down(): void
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('products');
+        Schema::dropIfExists('carts');
+        Schema::dropIfExists('cart_items');
     }
 };
