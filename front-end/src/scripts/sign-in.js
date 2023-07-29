6ef8e0d1112d07eb5abedb65698bcbd255bac9b4
Mostafa Kreidly <mostafa@Mostafas-MacBook-Air.local>
2023-07-29 18:59:19 +0300

    <FE> Placed input fields for signIn/signUp and did signIn fetch

---
+++ front-end/src/scripts/sign-in.js
@@ -0,0 +1,48 @@
+window.addEventListener('load', () => {
+  const email_in = document.getElementById('sign-in-email');
+  const password_in = document.getElementById('sign-in-password');
+  const button_sign_in = document.getElementById('sign-in-button');
+  const error_sign_in = document.getElementById('sign-in-error');
+  const name_up = document.getElementById('sign-up-name');
+  const email_up = document.getElementById('sign-up-email');
+  const password_up = document.getElementById('sign-up-password');
+
+  const button_sign_up = document.getElementById('sign-up-verify');
+
+  //form-validation
+  function checkEmpty(error_field, ...args) {
+    for (element of args) {
+      if (!element.value.trim()) {
+        error_field.innerText = 'Fields cannot be empty';
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  //   Sign in
+  button_sign_in.addEventListener('click', async (e) => {
+    if (checkEmpty(error_sign_in, email_in, password_in)) return;
+
+    const body = {
+      email: email_in.value,
+      password: password_in.value,
+    };
+    try {
+      console.log(body);
+      const response = await fetch('http://127.0.0.1:8000/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      });
+
+      localStorage.setItem('user_info', response);
+      window.location.href = '../index.html';
+    } catch (error) {
+      error_sign_in.innerText = 'Invalid Email or password';
+    }
+  });
+});
