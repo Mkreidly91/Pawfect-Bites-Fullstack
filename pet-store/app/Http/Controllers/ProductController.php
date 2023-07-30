db2b33f9fac502a51b4404534341472aafae05dd
Mostafa Kreidly <mostafa@Mostafas-MacBook-Air.local>
2023-07-30 13:21:42 +0300

    <BE> fixed products controller

--- pet-store/app/Http/Controllers/ProductController.php
+++ pet-store/app/Http/Controllers/ProductController.php
@@ -7,16 +7,13 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function getAll(Request $request, $productCategory = null, $userId = null)
+    public function getAll($category = null, $userId = null)
     {
-        // Get a query builder instance of the Product model with eager loading of category and favorites relationships
         $productsQuery = Product::with('category');
-
-        // Apply filters based on the provided $productCategory and $userId
-        if ($productCategory !== null) {
-            $productsQuery->whereHas('category', function ($query) use ($productCategory) {
-                $query->where('name', $productCategory);
-            });
+        // Apply filters based on the provided $category and $userId
+        if ($category !== null) {
+            $categoryId = intval($category);
+            $productsQuery->where('category_id', $categoryId);
         }
 
         if ($userId !== null) {
@@ -28,6 +25,9 @@ class ProductController extends Controller
         // Execute the query and retrieve the filtered products
         $products = $productsQuery->get();
         // Now you have a collection of products, each with its associated category and favorites
-        return response()->json(['data' => $products]);
+        return response()->json([
+            'data' => $products,
+            'category' => $category
+        ]);
     }
 }
--- pet-store/routes/api.php
+++ pet-store/routes/api.php
@@ -27,4 +27,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/products/{productCategory?}/{userId?}', [ProductController::class, 'getAll']);
+Route::get('products/{category?}/{userId?}', [ProductController::class, 'getAll']);
