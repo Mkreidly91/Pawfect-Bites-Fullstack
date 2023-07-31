96ba73fa7df2303a3c491e3d49dcfc04b7f732b2
Mostafa Kreidly <mostafa@Mostafas-MacBook-Air.local>
2023-07-31 14:54:00 +0300

    added select options to sidebar

--- front-end/src/scripts/dashboard.js
+++ front-end/src/scripts/dashboard.js
@@ -1,1 +1,5 @@
-document.addEventListener('click', () => {});
+document.addEventListener('click', () => {
+  const input_title = document.getElementById('title');
+  const input_description = document.getElementById('description');
+  const input_category = document.getElementById('category');
+});
